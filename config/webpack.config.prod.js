54a8ab938a9d093cb76efc0f5d0d8cccca55acc9
Sebastian Dragan <sebastian.dragan305@gmail.com>
2023-12-12 19:12:08 +0100

    init commit

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'js/[name]-[contenthash].js',
+    path: path.resolve(__dirname, '../', 'build'),
+  },
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]],
+          plugins: ['@babel/plugin-transform-class-properties'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/templates/template.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css',
+    }),
+  ],
+};
