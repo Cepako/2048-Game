9d8a3451b4a8e7a85fc5116287464762a58a2bcd
Sebastian Dragan <sebastian.dragan305@gmail.com>
2023-12-16 14:55:54 +0100

    add left arrow functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,22 +1,63 @@
-let score, board;
+let score,
+  board,
+  rows = 4,
+  columns = 4;
 
 function initGameBoard() {
   board = [
+    [2, 2, 0, 0],
     [2, 2, 2, 2],
-    [2, 2, 2, 2],
-    [4, 4, 8, 8],
-    [4, 4, 8, 8],
+    [4, 4, 0, 8],
+    [4, 0, 8, 8],
   ];
 
   const gamePanel = document.querySelector('.game-panel');
-  for (let r = 0; r < board.length; r++)
-    for (let c = 0; c < board[r].length; c++) {
-      const div = document.createElement('div');
-      div.classList.add('game-panel__tile');
-      div.dataset.value = board[r][c];
-      div.innerText = board[r][c];
-      gamePanel.appendChild(div);
+  for (let r = 0; r < rows; r++)
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement('div');
+      tile.id = `${r}-${c}`;
+      tile.classList.add('game-panel__tile');
+      let num = board[r][c];
+      updateTile(tile, num);
+      gamePanel.appendChild(tile);
     }
+}
+
+function updateTile(tile, num) {
+  tile.innerText = '';
+  tile.dataset.value = num;
+  if (num > 0) tile.innerText = num;
+}
+
+function filterZero(row) {
+  return row.filter((value) => value !== 0);
+}
+
+function slide(row) {
+  row = filterZero(row);
+  for (let c = 0; c < columns - 1; c++) {
+    if (row[c] === row[c + 1]) {
+      row[c] *= 2;
+      row[c + 1] = 0;
+      score += row[c];
+    }
+  }
+  row = filterZero(row);
+  while (row.length < columns) row.push(0);
+  return row;
+}
+
+function moveLeft() {
+  for (let r = 0; r < rows; r++) {
+    let row = board[r];
+    row = slide(row);
+    board[r] = row;
+    for (let c = 0; c < columns; c++) {
+      let tile = document.getElementById(`${r}-${c}`);
+      let num = board[r][c];
+      updateTile(tile, num);
+    }
+  }
 }
 
 function moveTiles(e) {
@@ -28,6 +69,7 @@ function moveTiles(e) {
     case 'ArrowRight':
       break;
     case 'ArrowLeft':
+      moveLeft();
       break;
   }
 }
