5b5f537400a148d32897ade9d9eef1f17c9302f4
Sebastian Dragan <sebastian.dragan305@gmail.com>
2023-12-16 13:51:06 +0100

    rebuild app and add board init function

--- src/app/app.js
+++ src/app/app.js
@@ -1,82 +1,27 @@
-const tiles = [...document.querySelectorAll('.game-panel__tile')];
+let score, board;
 
-function randomTile() {
-  let randomFlag = true,
-    randomTileIndex,
-    countUsedTiles = 0;
-  tiles.forEach((tile) => {
-    if (tile.innerText !== '') countUsedTiles++;
-  });
-  while (randomFlag) {
-    randomTileIndex = Math.floor(Math.random() * tiles.length);
-    if (tiles[randomTileIndex].innerText === '') randomFlag = !randomFlag;
-    else if (countUsedTiles === tiles.length) randomFlag = !randomFlag;
-  }
-  if (countUsedTiles < tiles.length) {
-    tiles[randomTileIndex].innerText = 2;
-    tiles[randomTileIndex].dataset.value = 2;
+function initGameBoard() {
+  board = [
+    [2, 2, 2, 2],
+    [2, 2, 2, 2],
+    [4, 4, 8, 8],
+    [4, 4, 8, 8],
+  ];
 
-    tiles[randomTileIndex].classList.add('tile-appear');
-
-    tiles[randomTileIndex].addEventListener(
-      'transitionend',
-      function () {
-        this.classList.remove('tile-appear');
-      },
-      { once: true }
-    );
-  }
-}
-
-document.querySelector('.header__reset').addEventListener('click', () => {
-  tiles.forEach((tile) => {
-    tile.innerText = '';
-    tile.dataset.value = '';
-  });
-  randomTile();
-  randomTile();
-});
-
-function changeTile(i, j, moveType) {
-  if (moveType === 'move') {
-    tiles[j].dataset.value = tiles[i].dataset.value;
-    tiles[j].innerText = tiles[i].innerText;
-    tiles[i].dataset.value = '';
-    tiles[i].innerText = '';
-  } else if (moveType === 'add') {
-    tiles[j].dataset.value = tiles[j].dataset.value * 2;
-    tiles[j].textContent = tiles[j].dataset.value;
-    tiles[i].dataset.value = '';
-    tiles[i].innerText = '';
-  }
-}
-
-function moveUp() {
-  let canGoUp = true;
-  for (let i = 4; i < tiles.length; i++) {
-    let j = i;
-    while (j >= 4) {
-      const currentTile = tiles[j],
-        aboveTile = tiles[j - 4];
-      if (aboveTile.dataset.value === '') {
-        changeTile(j, j - 4, 'move');
-      } else if (aboveTile.dataset.value === currentTile.dataset.value) {
-        changeTile(j, j - 4, 'add');
-        break;
-      } else {
-        canGoUp = !canGoUp;
-        break;
-      }
-      j -= 4;
+  const gamePanel = document.querySelector('.game-panel');
+  for (let r = 0; r < board.length; r++)
+    for (let c = 0; c < board[r].length; c++) {
+      const div = document.createElement('div');
+      div.classList.add('game-panel__tile');
+      div.dataset.value = board[r][c];
+      div.innerText = board[r][c];
+      gamePanel.appendChild(div);
     }
-  }
-  if (canGoUp) randomTile();
 }
 
 function moveTiles(e) {
   switch (e.key) {
     case 'ArrowUp':
-      moveUp();
       break;
     case 'ArrowDown':
       break;
@@ -87,7 +32,6 @@ function moveTiles(e) {
   }
 }
 
-document.addEventListener('keydown', moveTiles);
+document.addEventListener('keyup', moveTiles);
 
-randomTile();
-randomTile();
+document.addEventListener('DOMContentLoaded', initGameBoard);
